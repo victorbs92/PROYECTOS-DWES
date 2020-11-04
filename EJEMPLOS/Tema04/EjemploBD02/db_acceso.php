1fcdbdb2a28b500d9671d0466537b1b5c81c7345
victorbs <victorbs@alumnos.iesgalileo.es>
2020-11-04 09:58:52 +0100

    .

--- EJEMPLOS/Tema04/EjemploBD02/db_acceso.php
+++
@@ -1,6 +0,0 @@
- <?php
-       $conexion = new mysqli('localhost', 'root', 'admin', 'dwes'); 
-       print $conexion->server_info;
-
-        ?>
-
---
+++ PRUEBAS/Ejemplo/db_acceso.php
@@ -0,0 +1,6 @@
+ <?php
+       $conexion = new mysqli('localhost','dwes','abc123','dwes');
+       print $conexion->server_info;
+
+        ?>
+
