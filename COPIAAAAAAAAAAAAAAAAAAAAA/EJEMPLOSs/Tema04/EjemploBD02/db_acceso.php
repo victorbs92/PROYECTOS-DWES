89704e7e0e1abc07b095690d8eb578b91066bb91
Victor <victorbs@alumnos.iesgalileo.es>
2020-11-05 04:02:13 +0100

    .

--- COPIAAAAAAAAAAAAAAAAAAAAA/EJEMPLOSs/Tema04/EjemploBD02/db_acceso.php
+++
@@ -1,6 +0,0 @@
- <?php
-       $conexion = new mysqli('localhost','dwes','abc123','dwes');
-       print $conexion->server_info;
-
-        ?>
-
--- COPIAAAAAAAAAAAAAAAAAAAAA/EJERCICIOSs/Tema04/Ejercicio1.3-Funicular_MySQLi/db_acceso.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$conexion = new mysqli('localhost', 'dwes', 'abc123', 'funicular'); //("localhost", "usuario", "contraseña", "basedatos")
-print $conexion->server_info;
-?>
-
