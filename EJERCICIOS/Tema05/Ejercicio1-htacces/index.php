61f912e2101f257ea0eedf9f2e730f1ce4935d31
victorbs <victorbs@alumnos.iesgalileo.es>
2020-11-12 09:35:23 +0100

    .

---
+++ EJERCICIOS/Tema05/Ejercicio1-htacces/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>HOLA <?= $_SERVER['PHP_AUTH_USER'] ?></h1>
+        <?php
+        /*
+          echo "Nombre de usuario: " . $_SERVER['PHP_AUTH_USER'] . "<br />";
+          echo "Contraseña: " . $_SERVER['PHP_AUTH_PW'] . "<br />";
+          echo "Método de autentificación: " . $_SERVER['AUTH_TYPE'] . "<br />";
+
+
+          if (!isset($_SERVER['PHP_AUTH_USER'])) {
+          header('WWW-Authenticate: Basic Realm="Contenido restringido"');
+          header('HTTP/1.0 401 Unauthorized');
+          echo "Usuario no reconocido!";
+          exit();
+          }
+         */
+        /*
+          if ($_SERVER['PHP_AUTH_USER'] != 'dwes' || $_SERVER['PHP_AUTH_PW'] != 'abc123') {
+          header('WWW-Authenticate: Basic Realm="Contenido restringido');
+          header('HTTP/1.0 401 Unauthorized');
+          echo "Usuario no reconocido";
+          exit();
+          } else {
+          print "HOLA";
+          }
+         */
+        ?>
+    </body>
+</html>
