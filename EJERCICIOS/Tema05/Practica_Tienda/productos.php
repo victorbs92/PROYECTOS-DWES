bb8b4b7e4a7d6d9f518cee302dd39b0f7b17237d
Victor <victorbs@alumnos.iesgalileo.es>
2020-11-24 00:49:06 +0100

    .

--- EJERCICIOS/Tema05/Practica_Tienda/productos.php
+++ EJERCICIOS/Tema05/Practica_Tienda/productos.php
@@ -7,80 +7,133 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Productos</title>
     </head>
     <body>
-        <form id="productos" action="productos.php" method="post">
-            <fieldset>
-                <legend>
-                    <h1>PRODUCTOS</h1>
-                </legend>
-                <div>
-                    <label for="user">User: </label>
-                    <input type="text" id="user" name="user" required>
-                    <br>
-                    <br>
-                    <label for="pass">Pass: </label>
-                    <input type="password" id="pass" name="pass" required>
-                    <br>
-                    <br>
-                </div>
-                <div>
-                    <input type="submit" id="registrar" name="registrar" value="Registrar">
-                </div>
-            </fieldset>
+
+        <?php
+        if (isset($_SESSION['nombreUsuario'])) {//SI EL USUARIO SI SE HA AUTENTIFICADO CARGA LA PAGINA Y SU CONTENIDO
+            ?>
+            <input type = "submit" name = "cerrarSesion" value = "Cerrar Sesión">
+            <form id = "productos" action = "productos.php" method = "post">
+                <fieldset>
+                    <legend>
+                        <h1>PRODUCTOS</h1>
+                    </legend>
+
+                    <?php
+                    //incluimos el acceso a la BD
+                    include './db_acceso.php';
+
+                    //si tenemos conexion...
+                    if ($conexion) {
+
+                        //guardamos la consulta sql en una variable
+                        $sqlConsultaProductos = "SELECT * FROM productos";
+
+                        $resultadoConsulta = $conexion->query($sqlConsultaProductos);
+
+                        $arrayResultado = $resultadoConsulta->fetch_all(MYSQLI_ASSOC); //el resultado es un array que contiene un array por cada fila devuelta (array de arrays)(matriz)
+                        //$result->field_seek(0); //PARA DEVOLVER EL PUNTERO A LA POSICION QUE SE LE PASA COMO ARGUMENTO AL METODO FIELD_SEEK()!!!!!!!!!!!
+
+                        $arrayProductos = array(); //creamos un array numérico donde ir guardando todos los productos, cada clave será el idProducto y el valor será otro array con los datos del producto
+
+                        print ("<table border = 1>"); //creamos la tabla
+                        print ("<tr>");
+                        //CABECERA DE LA TABLA
+                        foreach ($arrayResultado[0] as $key => $value) {//para conocer las claves solo necesitamos una fila devuelta, en este caso usamos la primera.
+                            if ($key != "idProducto") {
+                                print "<th>$key</th>";
+                            }
+                        }
+                        print "<th>Añadir al carrito</th>";
+                        print ("</tr>");
+
+                        //CUERPO DE LA TABLA
+                        for ($index = 0; $index < count($arrayResultado); $index++) {//para recorrer una matriz necesitamos bucles anidados
+                            print ("<tr>");
+
+                            $producto = array(); //creamos un array por cada fila de la tabla
+
+                            foreach ($arrayResultado[$index] as $key => $value) {
+
+                                $producto[$key] = $value; //guardamos en el array asociativo las clave/valor de cada producto
+
+                                if ($key == 'imagen') {
+                                    print "<th><img border='0' width='100' height='100' src='./img/$value.jpg' ></a></th>";
+                                } elseif ($key != 'idProducto') {
+                                    print "<th>$value</th>";
+                                }
+                            }
+
+                            $idValue = $arrayResultado[$index]['idProducto']; //guardamos en una variable el valor del idProducto
+
+                            $arrayProductos[$idValue] = $producto; //Añadimos el array producto al array de arrays 
+
+                            print "<th><input type='submit' value='Añadir' name='añadir[$idValue]'></th>"; //el name será un array y su indice será el idValue
+                            print ("</tr>");
+                        }
+
+                        $resultadoConsulta->close(); //cerramos la consulta
+                        $conexion->close(); //cerramos la conexion
+                    }
+                    ?>
+                </fieldset>
+
+                <fieldset>
+                    <h1>CESTA</h1>
+                    <p>
+                        <?php
+                        if (isset($_POST['añadir'])) { //comprobamos si se ha enviado el formulario habiendo pulsado el boton de añadir
+                            $botonAñadirPulsado = array_key_first($_POST['añadir']); //guardamos en una variable la key del array del boton añadir que hemos pulsado
+
+                            if (isset($_SESSION['cesta'])) { //si cesta si que existe en la sesion --> hacemos todo esto para que pueda haber mas de un elemento en la cesta.
+                                $cesta = $_SESSION['cesta']; //creamos una variable llamada cesta donde guardamos el valor de cestaSession
+                                $cesta[] = $arrayProductos[$botonAñadirPulsado]; //añadimos a la variable el nuevo array con los datos del producto que hemos seleccionado al pinchar en el boton añadir
+                                $_SESSION['cesta'] = $cesta; //igualamos la cestaSession con la variable cesta a la que se le acaba de añadir un nuevo producto
+                            } else { //si cesta no existe en la sesion
+                                $cesta[] = $arrayProductos[$botonAñadirPulsado]; //creamos el array cesta y guardamos el producto del boton que hemos pinchado
+                                $_SESSION['cesta'] = $cesta; //creamos cestaSession y guardamos en ella el array anterior
+                            }
+
+                            print "<ul>"; //mostramos la cesta en una lista desordenada
+                            for ($index1 = 0; $index1 < count($_SESSION['cesta']); $index1++) {
+                                print "<li>";
+                                print "ID = " . $_SESSION['cesta'][$index1]['idProducto'];
+                                print " --- " . $_SESSION['cesta'][$index1]['nombreProducto'];
+                                print " --- " . $_SESSION['cesta'][$index1]['precio'] . "€";
+                                print "</li>";
+                            }
+
+                            print "</ul>";
+                        } else {
+                            print 'Aún no hay nada por aquí';
+                        }
+
+                        if (isset($_POST['comprar'])) { //si se ha pulsado el boton comprar
+                            if (!empty($_SESSION['cesta'])) { //si cestaSession  no esta vacia
+                            }
+                        }
+
+                        if (isset($_POST['vaciar'])) { //si se ha pulsado el boton vaciar carrito
+                            unset($_SESSION['cesta']);
+                        }
+                        ?>
+                    </p>
+                    <input type = "submit" name = "comprar" value = "Comprar">
+                    <input type = "submit" name = "vaciar" value = "Vaciar carrito">
+                </fieldset>
+                <br>
+                <br>
+
+            </form>
 
             <?php
-            //incluimos el acceso a la BD
-            include './db_acceso.php';
+        } else { //SI EL USUARIO NO SE HA AUTENTIFICADO
+            print "<h1>ERROR.</h1>";
+            print "<a href=registro.php>Login</a></td>";
+        }
+        ?>
 
-            //si tenemos conexion...
-            if ($conexion) {
-
-                //guardamos la consulta sql en una variable
-                $sqlConsultaProductos = "SHOW COLUMNS FROM productos";
-
-                //preparamos, ejecutamos la consulta y guardamos el resultado que devuelve (el nº de filas afectadas) en una variable
-                $resultado = $conexion->query($sqlConsultaProductos);
-
-                $nombreColumn = mysqli_fetch_assoc($resultado); //Guarda en un array el nombre de las columnas de la tabla a la que se hace la consulta
-               // print_r($nombreColumn);
-
-                $clavesNombreColumn = array_keys($nombreColumn); //Guarda en un array las claves del array anterior para poder recorrerlo
-                //print_r($clavesNombreColumn);
-//
-//                if ($resultado !== false) {
-//
-//                    print ("<table border = 1>"); //creamos la tabla
-//                    //CABECERA DE LA TABLA
-//                    print ("<tr>");
-//                    foreach ($clavesNombreColumn as $value) {
-////                        if ($value != "id" && $value != "imagen") {
-////                            print "<th>$value</th>";
-////                        }
-//                    }
-//                    print "<th>Borrar</th>";
-//                    print ("</tr>");
-//
-//                    //CUERPO DE LA TABLA
-//                    $resultado->execute();
-//                    while ($datos = $resultado->fetch()) {
-//                        print ("<tr>");
-////                        print ("<td>" . $datos['titulo'] . "</td>");
-////                        print ("<td>" . $datos['texto'] . "</td>");
-////                        print ("<td>" . $datos['categoria'] . "</td>");
-////                        print ("<td>" . $datos['fecha'] . "</td>");
-////                        print ("<td>" . "<input type='checkbox' name=chkBorrar[] value=$datos[id]>" . "</td>");
-//                        print ("</tr>");
-//                    }
-//                }
-//
-//                print ("</table>"); //cerramos la tabla
-//
-//                unset($conexion); //Cerramos la conexion
-            }
-            ?>
-
-        </form>
     </body>
 </html>
