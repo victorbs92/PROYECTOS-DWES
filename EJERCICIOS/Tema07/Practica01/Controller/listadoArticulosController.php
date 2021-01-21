f2b72b10e4d56a0354c76d2aca3d443c13f62893
victorbs <victorbs@alumnos.iesgalileo.es>
2021-01-21 10:08:16 +0100

    .

--- EJERCICIOS/Tema07/Practica01/Controller/listadoArticulosController.php
+++ EJERCICIOS/Tema07/Practica01/Controller/listadoArticulosController.php
@@ -3,31 +3,32 @@
 /* INCLUDES Y REQUIRES */
 require_once("../Config/Autoload.php");
 
-var_dump($_GET);
+
 
 /* SESION */
-if (isset($_GET['userSession'])) {
+if (isset($_GET['userSession'])) { //si en el get existe userSession crea una sesion
     Session::crearSesion($_GET['userSession']);
 }
 
 /* VARIABLES */
 
 
+if (isset($_POST['cerrarSesion'])) {
+    print("<br>");
+    print("AAAAA");
+} else if (isset($_POST['verArticulo'])) {
+    
+}
+
 
 if (isset($_SESSION['nombreUsuario'])) {//SI EL USUARIO SI SE HA AUTENTIFICADO CARGA LA PAGINA Y SU CONTENIDO
-    if (isset($_POST['cerrarSesion'])) {
-        print("<br>");
-        print("AAAAA");
-    } else if (isset($_POST['verArticulo'])) {
-        
-    } else {
-        include_once '../View/listadoArticulosView.php'; //incluye la vista para mostrarla en pantalla
-    }
+    include_once '../View/listadoArticulosView.php'; //incluye la vista para mostrarla en pantalla
 } else {
     if (isset($_SESSION)) {
+        print ("GGGGGGG");
         Session::eliminarSesion();
     }
-    
+
     //header("Location: ../Controller/errorController.php");
 }
 
