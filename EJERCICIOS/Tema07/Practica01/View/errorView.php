6e7f24a3fb25012946c7e9b9afe74eef16e17821
Victor <victorbs@alumnos.iesgalileo.es>
2021-01-21 04:54:32 +0100

    .

---
+++ EJERCICIOS/Tema07/Practica01/View/errorView.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>ERROR</title>
+    </head>
+    <body>
+
+        <h1>ERROR.</h1>
+
+        <form id="errorForm" action="../Controller/errorController.php" method="post">
+            <input type="submit" id="salir" name="salir" value="Salir">
+        </form>
+
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
